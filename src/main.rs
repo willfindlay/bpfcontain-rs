d8df16531115d77b86c024e0a93c27eebc2c921a
William Findlay <william@williamfindlay.com>
2020-12-29 18:19:24 -0500

    Implement daemon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use anyhow::{bail, Result};
+use anyhow::{anyhow, Result};
 use clap::{App, AppSettings, Arg, SubCommand};
+use simple_logger::SimpleLogger;
 
 mod subcommands;
 use subcommands::daemon;
@@ -7,17 +8,67 @@ use subcommands::daemon;
 fn main() -> Result<()> {
     let app = App::new("BPFContain")
         .version("0.0.1")
-        .about("TODO: description here")
+        .about("Container security with eBPF")
         .author("William Findlay <william@williamfindlay.com>")
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .global(true)
+                .help("Sets verbosity. Possible values are -v, -vv, or -vvv"),
+        )
         .setting(AppSettings::ArgRequiredElseHelp)
         .global_setting(AppSettings::ColoredHelp)
-        .subcommand(SubCommand::with_name("daemon").about("Control the BPFContain daemon."));
+        .subcommand(
+            SubCommand::with_name("daemon")
+                .about("Control the BPFContain daemon.")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .subcommand(
+                    SubCommand::with_name("start")
+                        .about("Start the daemon")
+                        .display_order(1),
+                )
+                .subcommand(
+                    SubCommand::with_name("stop")
+                        .about("Stop the daemon")
+                        .display_order(2),
+                )
+                .subcommand(
+                    SubCommand::with_name("restart")
+                        .about("Restart the daemon")
+                        .display_order(3),
+                ),
+        );
 
+    // Parse the arguments
     let args = app.get_matches();
 
-    if let Some(subargs) = args.subcommand_matches("daemon") {
-        return daemon::main(subargs);
+    // Set log level based on verbosity
+    // Level 0: Warning
+    // Level 1: Info
+    // Level 2: Debug
+    // Level 3: Trace
+    let log_level = match args.occurrences_of("v") {
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        3 | _ => log::LevelFilter::Trace,
+    };
+
+    // Initialize the logger
+    SimpleLogger::new().with_level(log_level).init()?;
+
+    // Dispatch to subcommand
+    let result = match args.subcommand() {
+        ("daemon", Some(args)) => daemon::main(args),
+        // TODO: match other subcommands
+        (unknown, _) => Err(anyhow!("Unknown subcommand {}", unknown)),
+    };
+
+    // Log errors if they bubble up
+    if let Err(e) = &result {
+        log::error!("Error: {}", e);
     }
 
-    Ok(())
+    result
 }
--- src/subcommands/daemon.rs
+++ src/subcommands/daemon.rs
@@ -1,8 +1,123 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::ArgMatches;
+use daemonize::Daemonize;
+use std::fs::{create_dir_all, metadata, set_permissions, File, OpenOptions};
+use std::io::Read;
+use std::os::unix::fs::PermissionsExt;
+
+static PIDFILE: &str = "/run/bpfcontain.pid";
+static LOGFILE: &str = "/var/log/bpfcontain.log";
+static WORKDIR: &str = "/var/lib/bpfcontain";
 
 pub fn main(args: &ArgMatches) -> Result<()> {
-    println!("Hello daemon world! My args are: {:?}", args);
+    let result = match args.subcommand() {
+        ("start", Some(_args)) => start_daemon(),
+        ("restart", Some(_args)) => restart_daemon(),
+        ("stop", Some(_args)) => stop_daemon(),
+        _ => bail!("Bad subcommand name"),
+    };
+
+    result
+}
+
+/// Starts the daemon.
+fn start_daemon() -> Result<()> {
+    log::info!("Starting daemon...");
+
+    // Open the log file
+    let stdout = OpenOptions::new().create(true).append(true).open(LOGFILE)?;
+    let stderr = OpenOptions::new().create(true).append(true).open(LOGFILE)?;
+
+    // Create workdir and set permissions to rwxr-xr-t
+    create_dir_all(WORKDIR)?;
+    let mut perms = metadata(WORKDIR)?.permissions();
+    perms.set_mode(0o1755);
+    set_permissions(WORKDIR, perms)?;
+
+    // Set up the daemon
+    let daemonize = Daemonize::new()
+        .pid_file(PIDFILE)
+        .user("nobody")
+        .stdout(stdout)
+        .stderr(stderr)
+        .working_directory(WORKDIR)
+        .exit_action(|| log::info!("Started the daemon!"));
+
+    // Try to start the daemon
+    match daemonize.start() {
+        Ok(_) => log::info!("Started the daemon!"),
+        Err(e) => {
+            bail!("Failed to start the daemon: {}", e);
+        }
+    }
+
+    // FIXME: replace this with the actual BPF program work loop
+    std::thread::sleep(std::time::Duration::new(10, 0));
 
     Ok(())
+}
+
+/// Stops the daemon by parsing the daemon's [`PIDFILE`] and sending a `SIGTERM`
+/// using kill(2).
+fn stop_daemon() -> Result<()> {
+    log::info!("Stopping daemon...");
+
+    // Parse pid from pidfile
+    let pid: i32 = {
+        // Open pidfile for reading
+        let mut pidfile = match File::open(PIDFILE) {
+            Ok(file) => file,
+            Err(e) => bail!("Failed to open pidfile: {}", e),
+        };
+
+        // Read contents of pidfile
+        let mut contents = String::new();
+        match pidfile.read_to_string(&mut contents) {
+            Ok(_) => {}
+            Err(e) => bail!("Unable to read pidfile: {}", e),
+        };
+
+        // Parse contents into a pid
+        match contents.parse() {
+            Ok(pid) => pid,
+            Err(e) => bail!("Failed to parse pid: {}", e),
+        }
+    };
+
+    // Invoke kill(2) to send a SIGTERM to the process running under pid
+    if unsafe { libc::kill(pid, libc::SIGTERM) } != 0 {
+        let err = std::io::Error::last_os_error();
+        bail!("Failed to kill the daemon ({}): {}", pid, err);
+    };
+
+    Ok(())
+}
+
+/// Restarts the daemon by invoking [`stop_daemon`] followed by
+/// [`start_daemon`]. [`stop_daemon`] is allowed to fail with a warning.
+///
+/// FIXME: This is racy because we need to wait for the pidfile to be unlocked
+/// before we can start the daemon. As a crude workaround, we currently
+/// sleep for 1 second after a successful call to [`stop_daemon`].
+///
+/// This behaviour should be changed in future versions to wait for the file to
+/// be unlocked.
+fn restart_daemon() -> Result<()> {
+    log::info!("Restarting daemon...");
+
+    // Try to stop the daemon
+    match stop_daemon() {
+        Ok(_) => {
+            // FIXME: Should poll to see if the process has actually stopped
+            std::thread::sleep(std::time::Duration::new(1, 0));
+        }
+        Err(e) => {
+            log::warn!(
+                "Unable to stop the daemon while restarting (daemon may not be running): {}",
+                e
+            );
+        }
+    }
+
+    start_daemon()
 }
