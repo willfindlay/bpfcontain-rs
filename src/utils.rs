2916e7b65024f44bc7e06aaba77d79f6831f2e9e
William Findlay <william@williamfindlay.com>
2020-12-29 11:59:54 -0500

    Add bump_memlock_rlimit utlity function

---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+use anyhow::{bail, Result};
+
+/// Bump the rlimit for memlock up to full capacity.
+/// This is required to load even reasonably sized eBPF maps.
+///
+/// Borrowed this function from [the libbpf-rf
+/// docs](https://github.com/libbpf/libbpf-rs/blob/master/examples/runqslower/src/main.rs).
+pub fn bump_memlock_rlimit() -> Result<()> {
+    let rlimit = libc::rlimit {
+        rlim_cur: 128 << 20,
+        rlim_max: 128 << 20,
+    };
+
+    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
+        bail!("Failed to increase rlimit");
+    }
+
+    Ok(())
+}
