132d583788f885efe4c1ae5a88ebbd70258d4816
William Findlay <william@williamfindlay.com>
2020-12-30 14:29:58 -0500

    Add utility functions to parse symbol relative offset from ELF files

--- src/utils.rs
+++ src/utils.rs
@@ -6,6 +6,9 @@
 // Dec. 29, 2020  William Findlay  Created this.
 
 use anyhow::{bail, Result};
+use goblin::Object;
+use std::fs;
+use std::path::Path;
 
 /// Bump the rlimit for memlock up to full capacity.
 /// This is required to load even reasonably sized eBPF maps.
@@ -23,4 +26,63 @@ pub fn bump_memlock_rlimit() -> Result<()> {
     }
 
     Ok(())
+}
+
+/// Locate a symbol `symbol_name` in an ELF binary represented by the Goblin
+/// Elf struct `elf`.
+pub fn get_symbol(elf: &goblin::elf::Elf, symbol_name: &str) -> Result<goblin::elf::Sym> {
+    // Grab the symbol table and string table
+    let symtab = &elf.syms;
+    let strtab = &elf.strtab;
+
+    // Locate the
+    let sym = symtab.iter().find(|sym| {
+        if let Some(sym) = strtab.get(sym.st_name) {
+            if let Ok(name) = sym {
+                if name == symbol_name {
+                    return true;
+                }
+            }
+        }
+        false
+    });
+
+    match sym {
+        Some(sym) => Ok(sym),
+        None => bail!("Failed to find symbol {}", symbol_name),
+    }
+}
+
+/// Get the offset of a symbol `symbol_name` relative to .text in an ELF binary
+/// located at `binary_path`.
+pub fn get_symbol_offset(binary_path: &str, symbol_name: &str) -> Result<usize> {
+    let path = Path::new(binary_path);
+    let buffer = fs::read(path)?;
+
+    // Parse the ELF file
+    let elf = match Object::parse(&buffer)? {
+        Object::Elf(elf) => elf,
+        _ => bail!("Failed to parse ELF file {}", binary_path),
+    };
+
+    // Find the relative offset of symbol
+    let symbol = get_symbol(&elf, symbol_name)?;
+    let offset = symbol.st_value + elf.entry;
+
+    Ok(offset as usize)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_symbol() -> Result<()> {
+        todo!()
+    }
+
+    #[test]
+    fn test_get_symbol_offset() -> Result<()> {
+        todo!()
+    }
 }
