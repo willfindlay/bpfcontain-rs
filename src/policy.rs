0a4162325f6a60e4b71b002d4700b5d53a7acad0
William Findlay <william@williamfindlay.com>
2021-01-01 20:32:30 -0500

    Touch new files

---
+++ src/policy.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2
+//
+// BPFContain - Container security with eBPF
+// Copyright (C) 2020  William Findlay
+//
+// Dec. 29, 2020  William Findlay  Created this.
+
+use crate::structs::*;
+use anyhow::{bail, Result};
---
+++ src/subcommands/run.rs
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2
+//
+// BPFContain - Container security with eBPF
+// Copyright (C) 2020  William Findlay
+//
+// Dec. 29, 2020  William Findlay  Created this.
+
+use anyhow::{bail, Result};
+use clap::ArgMatches;
+use std::fs::File;
+use std::io::Read;
+
+pub fn main(args: &ArgMatches) -> Result<()> {
+    todo!()
+}
