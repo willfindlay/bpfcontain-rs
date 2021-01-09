891467d1f6fe652a1c798de2fb28c177b14a52d5
William Findlay <william@williamfindlay.com>
2021-01-09 10:03:00 -0500

    Document benchmarks

--- benches/overhead.rs
+++ benches/overhead.rs
@@ -5,16 +5,15 @@
 //
 // Dec. 29, 2020  William Findlay  Created this.
 
-use std::fs::{remove_file, File};
-use std::io::Write;
 use std::path::Path;
 
 use bpfcontain::bpf_program::{load_bpf_program, load_policy_str, BpfcontainSkelBuilder};
 use criterion::{black_box, criterion_group, criterion_main, Criterion, PlotConfiguration};
 use osbench_rs::{create_files, create_processes, create_threads};
 
-fn bench_os_overhead(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Writes");
+/// Benchmark create/delete files overhead with and without bpfcontain.
+fn bench_files_overhead(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Files");
 
     group.bench_function("no bpfcontain", |b| {
         b.iter(|| create_files(Path::new("/tmp")))
@@ -25,10 +24,12 @@ fn bench_os_overhead(c: &mut Criterion) {
 
     group.bench_function("bpfcontain", |b| b.iter(|| create_files(Path::new("/tmp"))));
 
+    // Manually drop skel here to avoid leaking into other tests
     drop(skel);
     group.finish();
 }
 
+/// Benchmark spawn/wait processes overhead with and without bpfcontain.
 fn bench_procs_overhead(c: &mut Criterion) {
     let mut group = c.benchmark_group("Processes");
     group.bench_function("no bpfcontain", |b| b.iter(|| create_processes()));
@@ -38,10 +39,12 @@ fn bench_procs_overhead(c: &mut Criterion) {
 
     group.bench_function("bpfcontain", |b| b.iter(|| create_processes()));
 
+    // Manually drop skel here to avoid leaking into other tests
     drop(skel);
     group.finish();
 }
 
+/// Benchmark spawn/join threads overhead with and without bpfcontain.
 fn bench_threads_overhead(c: &mut Criterion) {
     let mut group = c.benchmark_group("Threads");
     group.bench_function("no bpfcontain", |b| b.iter(|| create_threads()));
@@ -51,13 +54,14 @@ fn bench_threads_overhead(c: &mut Criterion) {
 
     group.bench_function("bpfcontain", |b| b.iter(|| create_threads()));
 
+    // Manually drop skel here to avoid leaking into other tests
     drop(skel);
     group.finish();
 }
 
 criterion_group!(
     osbench,
-    bench_os_overhead,
+    bench_files_overhead,
     bench_procs_overhead,
     bench_threads_overhead
 );
