16530c86dea145c5cfd2d0a9296c31fbf02b6624
William Findlay <william@williamfindlay.com>
2021-01-09 09:53:31 -0500

    Add some benchmarking

--- benches/overhead.rs
+++ benches/overhead.rs
@@ -5,22 +5,60 @@
 //
 // Dec. 29, 2020  William Findlay  Created this.
 
-use anyhow::Result;
+use std::fs::{remove_file, File};
+use std::io::Write;
+use std::path::Path;
+
 use bpfcontain::bpf_program::{load_bpf_program, load_policy_str, BpfcontainSkelBuilder};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::process::Command;
+use criterion::{black_box, criterion_group, criterion_main, Criterion, PlotConfiguration};
+use osbench_rs::{create_files, create_processes, create_threads};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
+fn bench_os_overhead(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Writes");
+
+    group.bench_function("no bpfcontain", |b| {
+        b.iter(|| create_files(Path::new("/tmp")))
+    });
+
+    let mut skel_builder = BpfcontainSkelBuilder::default();
+    let skel = load_bpf_program(&mut skel_builder, false).expect("Failed to load BPF program");
+
+    group.bench_function("bpfcontain", |b| b.iter(|| create_files(Path::new("/tmp"))));
+
+    drop(skel);
+    group.finish();
 }
 
-fn bench_fib(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+fn bench_procs_overhead(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Processes");
+    group.bench_function("no bpfcontain", |b| b.iter(|| create_processes()));
+
+    let mut skel_builder = BpfcontainSkelBuilder::default();
+    let skel = load_bpf_program(&mut skel_builder, false).expect("Failed to load BPF program");
+
+    group.bench_function("bpfcontain", |b| b.iter(|| create_processes()));
+
+    drop(skel);
+    group.finish();
 }
 
-criterion_group!(benches, bench_fib);
-criterion_main!(benches);
+fn bench_threads_overhead(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Threads");
+    group.bench_function("no bpfcontain", |b| b.iter(|| create_threads()));
+
+    let mut skel_builder = BpfcontainSkelBuilder::default();
+    let skel = load_bpf_program(&mut skel_builder, false).expect("Failed to load BPF program");
+
+    group.bench_function("bpfcontain", |b| b.iter(|| create_threads()));
+
+    drop(skel);
+    group.finish();
+}
+
+criterion_group!(
+    osbench,
+    bench_os_overhead,
+    bench_procs_overhead,
+    bench_threads_overhead
+);
+criterion_main!(osbench);
