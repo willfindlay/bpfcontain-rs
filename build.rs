c51a60d60fa5e6c7e0d2dd883bcb526a0a208253
William Findlay <william@williamfindlay.com>
2021-01-19 14:37:14 -0500

    Build improvements

--- build.rs
+++ build.rs
@@ -9,13 +9,15 @@ use std::process::Command;
 
 fn main() {
     // Re-run build if our header file(s) has changed
+    println!("cargo:rerun-if-changed=bindings.h");
     println!("cargo:rerun-if-changed=src/include/libbpfcontain.h");
     println!("cargo:rerun-if-changed=src/include/structs.h");
     println!("cargo:rerun-if-changed=src/bpf/bpfcontain.bpf.c");
     println!("cargo:rerun-if-changed=src/bpf/bpfcontain.h");
     println!("cargo:rerun-if-changed=src/bpf/kernel_defs.h");
     println!("cargo:rerun-if-changed=src/bpf/maps.h");
-    println!("cargo:rerun-if-changed=src/bpf/vmlinux.h");
+
+    // TODO: test for libbpfcontain
 
     // Generate bindings
     let bindings = bindgen::builder()
@@ -34,6 +36,15 @@ fn main() {
     bindings
         .write_to_file("src/libbpfcontain/bindings.rs")
         .expect("Failed to save bindings");
+
+    // Make vmlinux if we don't have a good enough version
+    // TODO: This command can be allowed to fail if we already have an existing vmlinux.h
+    let status = Command::new("make")
+        .arg("vmlinux")
+        .current_dir("src/bpf")
+        .status()
+        .expect("Failed to run make");
+    assert!(status.success(), "Failed to update vmlinux.h");
 
     // Run cargo-libbpf-build
     let status = Command::new("cargo")
