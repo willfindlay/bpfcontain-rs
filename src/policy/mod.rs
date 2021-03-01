7d84a078e6a9560673cc4e2ec92fcc903a67abde
William Findlay <william@williamfindlay.com>
2021-02-28 20:34:59 -0500

    Partial policy implementation

---
+++ src/policy/mod.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// BPFContain - Container security with eBPF
+// Copyright (C) 2020  William Findlay
+//
+// Dec. 29, 2020  William Findlay  Created this.
+
+mod policy;
+mod rules;
+
+pub use policy::Policy;
