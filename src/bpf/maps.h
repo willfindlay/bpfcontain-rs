855f78993de9e213206111bbc8040b5df4085ba7
William Findlay <william@williamfindlay.com>
2021-04-22 14:02:45 -0400

    Add automatic map pinning

--- src/bpf/maps.h
+++ src/bpf/maps.h
@@ -43,141 +43,142 @@ bpf_map_lookup_or_try_init(void *map, const void *key, const void *val)
  * ========================================================================= */
 
 /* Declare a BPF ringbuf map @NAME with 2^(@PAGES) size */
-#define BPF_RINGBUF(NAME, PAGES)                         \
+#define BPF_RINGBUF(NAME, PAGES, PIN)                    \
     struct {                                             \
         __uint(type, BPF_MAP_TYPE_RINGBUF);              \
         __uint(max_entries, ((1 << PAGES) * PAGE_SIZE)); \
+        __uint(pinning, PIN);                            \
     } NAME SEC(".maps")
 
-#define BPF_PERFBUF(NAME)                            \
+#define BPF_PERFBUF(NAME, PIN)                       \
     struct {                                         \
         __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY); \
         __uint(key_size, sizeof(u32));               \
         __uint(value_size, sizeof(u32));             \
+        __uint(pinning, PIN);                        \
     } NAME SEC(".maps");
 
 /* Declare a BPF hashmap @NAME with key type @KEY, value type @VALUE, and @SIZE
  * max entries. The map creation flags may be specified with @FLAGS. */
-#define BPF_HASH(NAME, KEY, VALUE, SIZE, FLAGS) \
-    struct {                                    \
-        __uint(type, BPF_MAP_TYPE_HASH);        \
-        __uint(max_entries, SIZE);              \
-        __type(key, KEY);                       \
-        __type(value, VALUE);                   \
-        __uint(map_flags, FLAGS);               \
+#define BPF_HASH(NAME, KEY, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                         \
+        __uint(type, BPF_MAP_TYPE_HASH);             \
+        __uint(max_entries, SIZE);                   \
+        __type(key, KEY);                            \
+        __type(value, VALUE);                        \
+        __uint(map_flags, FLAGS);                    \
+        __uint(pinning, PIN);                        \
     } NAME SEC(".maps")
 
 /* Declare an LRU BPF hashmap @NAME with key type @KEY, value type @VALUE, and
  * @SIZE max entries. The map creation flags may be specified with @FLAGS. */
-#define BPF_LRU_HASH(NAME, KEY, VALUE, SIZE, FLAGS) \
-    struct {                                        \
-        __uint(type, BPF_MAP_TYPE_LRU_HASH);        \
-        __uint(max_entries, SIZE);                  \
-        __type(key, KEY);                           \
-        __type(value, VALUE);                       \
-        __uint(map_flags, FLAGS);                   \
+#define BPF_LRU_HASH(NAME, KEY, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                             \
+        __uint(type, BPF_MAP_TYPE_LRU_HASH);             \
+        __uint(max_entries, SIZE);                       \
+        __type(key, KEY);                                \
+        __type(value, VALUE);                            \
+        __uint(map_flags, FLAGS);                        \
+        __uint(pinning, PIN);                            \
     } NAME SEC(".maps")
 
 /* Declare a per-cpu BPF hashmap @NAME with key type @KEY, value type @VALUE,
  * and
  * @SIZE max entries. The map creation flags may be specified with @FLAGS. */
-#define BPF_PERCPU_HASH(NAME, KEY, VALUE, SIZE, FLAGS) \
-    struct {                                           \
-        __uint(type, BPF_MAP_TYPE_PERCPU_HASH);        \
-        __uint(max_entries, SIZE);                     \
-        __type(key, KEY);                              \
-        __type(value, VALUE);                          \
-        __uint(map_flags, FLAGS);                      \
+#define BPF_PERCPU_HASH(NAME, KEY, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                                \
+        __uint(type, BPF_MAP_TYPE_PERCPU_HASH);             \
+        __uint(max_entries, SIZE);                          \
+        __type(key, KEY);                                   \
+        __type(value, VALUE);                               \
+        __uint(map_flags, FLAGS);                           \
+        __uint(pinning, PIN);                               \
     } NAME SEC(".maps")
 
 /* Declare a per-cpu LRU BPF hashmap @NAME with key type @KEY, value type
  * @VALUE, and @SIZE max entries. The map creation flags may be specified with
  * @FLAGS. */
-#define BPF_LRU_PERCPU_HASH(NAME, KEY, VALUE, SIZE, FLAGS) \
-    struct {                                               \
-        __uint(type, BPF_MAP_TYPE_LRU_PERCPU_HASH);        \
-        __uint(max_entries, SIZE);                         \
-        __type(key, KEY);                                  \
-        __type(value, VALUE);                              \
-        __uint(map_flags, FLAGS);                          \
+#define BPF_LRU_PERCPU_HASH(NAME, KEY, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                                    \
+        __uint(type, BPF_MAP_TYPE_LRU_PERCPU_HASH);             \
+        __uint(max_entries, SIZE);                              \
+        __type(key, KEY);                                       \
+        __type(value, VALUE);                                   \
+        __uint(map_flags, FLAGS);                               \
+        __uint(pinning, PIN);                                   \
     } NAME SEC(".maps")
 
 /* Declare a BPF array @NAME with value type @VALUE, and @SIZE max entries.
  * The map creation flags may be specified with @FLAGS. */
-#define BPF_ARRAY(NAME, VALUE, SIZE, FLAGS) \
-    struct {                                \
-        __uint(type, BPF_MAP_TYPE_ARRAY);   \
-        __uint(max_entries, SIZE);          \
-        __type(key, unsigned int);          \
-        __type(value, VALUE);               \
-        __uint(map_flags, FLAGS);           \
+#define BPF_ARRAY(NAME, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                     \
+        __uint(type, BPF_MAP_TYPE_ARRAY);        \
+        __uint(max_entries, SIZE);               \
+        __type(key, unsigned int);               \
+        __type(value, VALUE);                    \
+        __uint(map_flags, FLAGS);                \
+        __uint(pinning, PIN);                    \
     } NAME SEC(".maps")
 
 /* Declare a per-cpu BPF array @NAME with value type @VALUE, and @SIZE max
  * entries.  The map creation flags may be specified with @FLAGS. */
-#define BPF_PERCPU_ARRAY(NAME, VALUE, SIZE, FLAGS) \
-    struct {                                       \
-        __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);   \
-        __uint(max_entries, SIZE);                 \
-        __type(key, unsigned int);                 \
-        __type(value, VALUE);                      \
-        __uint(map_flags, FLAGS);                  \
-    } NAME SEC(".maps")
-
-/* Declare a cgroup storage map @NAME with value type @VALUE.
- * The map creation flags may be specified with @FLAGS. */
-#define BPF_CGROUP_STORAGE(NAME, VALUE, FLAGS)      \
-    struct {                                        \
-        __uint(type, BPF_MAP_TYPE_CGROUP_STORAGE);  \
-        __uint(max_entries, 0);                     \
-        __type(key, struct bpf_cgroup_storage_key); \
-        __type(value, VALUE);                       \
-        __uint(map_flags, FLAGS);                   \
+#define BPF_PERCPU_ARRAY(NAME, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                            \
+        __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);        \
+        __uint(max_entries, SIZE);                      \
+        __type(key, unsigned int);                      \
+        __type(value, VALUE);                           \
+        __uint(map_flags, FLAGS);                       \
+        __uint(pinning, PIN);                           \
     } NAME SEC(".maps")
 
 /* Declare a cgroup fd array @NAME with @SIZE max entries. The map creation
  * flags may be specified with @FLAGS. To populate the map, userspace should
  * obtain a cgroup-backed fd by calling open(2) on a cgroup directory. This fd
  * can then be inserted into the map from userspace. */
-#define BPF_CGROUP_ARRAY(NAME, SIZE, FLAGS)       \
+#define BPF_CGROUP_ARRAY(NAME, SIZE, PIN, FLAGS)  \
     struct {                                      \
         __uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);  \
         __uint(max_entries, SIZE);                \
         __uint(key_size, sizeof(unsigned int));   \
         __uint(value_size, sizeof(unsigned int)); \
         __uint(map_flags, FLAGS);                 \
+        __uint(pinning, PIN);                     \
     } NAME SEC(".maps")
 
 /* Declare a BPF stack @NAME with value type @VALUE, and @SIZE max entries.
  * The map creation flags may be specified with @FLAGS. */
-#define BPF_STACK(NAME, VALUE, SIZE, FLAGS) \
-    struct {                                \
-        __uint(type, BPF_MAP_TYPE_STACK);   \
-        __uint(max_entries, SIZE);          \
-        __uint(key_size, 0);                \
-        __uint(value_size, sizeof(VALUE));  \
-        __uint(map_flags, FLAGS);           \
+#define BPF_STACK(NAME, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                     \
+        __uint(type, BPF_MAP_TYPE_STACK);        \
+        __uint(max_entries, SIZE);               \
+        __uint(key_size, 0);                     \
+        __uint(value_size, sizeof(VALUE));       \
+        __uint(map_flags, FLAGS);                \
+        __uint(pinning, PIN);                    \
     } NAME SEC(".maps")
 
 /* Declare a BPF queue @NAME with value type @VALUE, and @SIZE max entries.
  * The map creation flags may be specified with @FLAGS. */
-#define BPF_QUEUE(NAME, VALUE, SIZE, FLAGS) \
-    struct {                                \
-        __uint(type, BPF_MAP_TYPE_QUEUE);   \
-        __uint(max_entries, SIZE);          \
-        __uint(key_size, 0);                \
-        __uint(value_size, sizeof(VALUE));  \
-        __uint(map_flags, FLAGS);           \
+#define BPF_QUEUE(NAME, VALUE, SIZE, PIN, FLAGS) \
+    struct {                                     \
+        __uint(type, BPF_MAP_TYPE_QUEUE);        \
+        __uint(max_entries, SIZE);               \
+        __uint(key_size, 0);                     \
+        __uint(value_size, sizeof(VALUE));       \
+        __uint(map_flags, FLAGS);                \
+        __uint(pinning, PIN);                    \
     } NAME SEC(".maps")
 
 /* Declare an inode storage map @NAME with value type @VALUE.
  * The map creation flags may be specified with @FLAGS. */
-#define BPF_INODE_STORAGE(NAME, VALUE, FLAGS)           \
+#define BPF_INODE_STORAGE(NAME, VALUE, PIN, FLAGS)      \
     struct {                                            \
         __uint(type, BPF_MAP_TYPE_INODE_STORAGE);       \
         __uint(map_flags, (FLAGS | BPF_F_NO_PREALLOC)); \
         __type(key, int);                               \
         __type(value, VALUE);                           \
+        __uint(pinning, PIN);                           \
     } NAME SEC(".maps")
 
 #endif /* ifndef MAPS_H */
