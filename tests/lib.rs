8afc2d5331629f1734906f68c4fc5631e9a47876
William Findlay <william@williamfindlay.com>
2021-05-04 17:38:16 -0400

    Move integration tests into their own module

---
+++ tests/lib.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// BPFContain - Container security with eBPF
+// Copyright (C) 2020  William Findlay
+//
+// May 3, 2020  William Findlay  Created this.
+
+#![cfg(test)]
+
+mod policy_enforcement;
