ebb1bd2f89d0967ff5de83b7915f260e40290fe5
William Findlay <william@williamfindlay.com>
2021-09-20 16:26:50 -0400

    types: touch ipc

---
+++ src/types/ipc.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// BPFContain - Container security with eBPF
+// Copyright (C) 2020  William Findlay
+//
+// Dec. 29, 2020  William Findlay  Created this.
